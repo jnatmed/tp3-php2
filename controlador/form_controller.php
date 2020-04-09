6d94a61740f313b06c59280a256a52a09cedc94b
jnatmed <juanmannat@gmail.com>
2020-04-09 04:25:44 -0300

    Terminando el punto 1. Falta verificar que el campo horario pueda mostrar dos digitos osea "00" en ese caso y que contemple la hora 17:00

--- controlador/form_controller.php
+++ controlador/form_controller.php
@@ -15,19 +15,19 @@ class form_controller
     {
         $this->agregar_dato('Nombre del Paciente','required','nombre','[a-zA-Z]+');
         $this->agregar_dato('Email', 'required','email','[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$');
-        $this->agregar_dato('Telefono', 'required','[0-9]');
-        $this->agregar_dato('Edad', 'edad','[0-9]');
-        $this->agregar_dato('Talla de calzado','calzado','20-45');
-        $this->agregar_dato('Altura', 'altura','1-3');
+        $this->agregar_dato('Telefono', 'required','telefono','/^\(?\d{2}\)?[\s\.-]?\d{4}[\s\.-]?\d{4}$/');
+        $this->agregar_dato('Edad', '', 'edad','18-60');
+        $this->agregar_dato('Talla de calzado', '', 'calzado','20-45');
+        $this->agregar_dato('Altura', '','altura','1-3');
         $this->agregar_dato('Fecha de nacimiento', 'required','date');
         $this->agregar_dato('Color de pelo','required','color');
         $this->agregar_dato('Fecha del turno', 'required','date');
-        $this->agregar_dato('Horario del turno', 'horario_turno','8-17-15');
+        $this->agregar_dato('Horario del turno', '', 'horario_turno','8-17-15');
         
         // echo "<pre>";
-        // var_dump($this->lista_datos[0]['obligatorio']);
-        // var_dump($this->lista_datos[0]['obligatorio']);
-        exit(0);
+        // var_dump($this->lista_datos[5]['tipo']);
+        // var_dump($this->lista_datos[5]['nombre_campo']);
+        // exit(0);
 
         $this->listar_datos();
     }
--- views/form.persona.view.php
+++ views/form.persona.view.php
@@ -7,16 +7,35 @@
     </header>
     <main>
         <form action="/save_data" method = 'POST'>
-          <?php foreach($this->lista_datos as $id_dato => $obligatorio):?>  
+          <?php foreach($this->lista_datos as $id_campo => $campo):?>  
             <p>
-            <label for="<?= $id_dato?>"><?= $id_dato?></label>
-            <?php if ($obligatorio == true)
-              {$requerido = "required";
-              }else{$requerido = "";}?>
-            <?php if($tipo_restriccion == 'altura'){?>  
-              <input type="text" name='<?= $id_dato?>' id='<?= $id_dato?>' <?= $requerido?>>
+            <label for="<?= $campo['nombre_campo']?>"><?= $campo['nombre_campo']?></label>
+            <?php if($campo['tipo'] == 'date'){ ?>  
+              <input type="date" name='date' id='date' <?= $campo['obligatorio']?>>
+            <?php }else if($campo['tipo'] == 'email'){ ?>  
+              <input type="text" name='<?= $campo['nombre_campo']?>' id='<?= $campo['nombre_campo']?>' pattern = <?= $campo['restriccion']?> tittle = "ej: pepe@servidor.com" <?= $campo['obligatorio']?>>
+            <?php }else if($campo['tipo'] == 'telefono'){ ?>  
+              <input type="text" name='<?= $campo['nombre_campo']?>' id='<?= $campo['nombre_campo']?>' pattern = <?= $campo['restriccion']?> tittle = "ej: 11 3438 7233" <?= $campo['obligatorio']?>>
+            <?php }else if($campo['tipo'] == 'altura'){ ?>  
+              <input type="range" name='<?= $campo['nombre_campo']?>' id='<?= $campo['nombre_campo']?>' min="100" max="280" <?= $campo['obligatorio']?>>
+            <?php }else if($campo['tipo'] == 'calzado' || $campo['tipo'] == 'edad' ){ 
+              list($min, $max) = explode("-",$campo['restriccion']);
+              ?>  
+              <input type="number" name='<?= $campo['nombre_campo']?>' id='<?= $campo['nombre_campo']?>' min="<?= $min?>" max="<?= $max?>" <?= $campo['obligatorio']?>>
+            <?php }else if($campo['tipo'] == 'color'){ ?>  
+              <input type="color" name='<?= $campo['nombre_campo']?>' id='<?= $campo['nombre_campo']?>' value="#ff0000" <?= $campo['obligatorio']?>>
+            <?php }else if($campo['tipo'] == 'horario_turno'){ ?>  
+              <select name="<?= $campo['nombre_campo']?>">
+               <?php 
+                  list($desde, $hasta, $intervalo) = explode("-", $campo['restriccion']);
+                  for ($hora = $desde; $hora < $hasta; $hora++) {
+                    for($min = 00; $min < 60; $min = $min + $intervalo) { ?>
+                        <option value="<?= $hora?>:<?= $min?>"><?= $hora?>:<?= $min?></option>
+                <?php  } } ?>               
+                </select>   
+
             <?php }else{ ?>
-              <input type="text" name='<?= $id_dato?>' id='<?= $id_dato?>' <?= $requerido?>>
+              <input type="text" name='<?= $campo['nombre_campo']?>' id='<?= $campo['nombre_campo']?>' <?= $campo['obligatorio']?>>
             <?php } ?>                
             </p>
             <?php  endforeach?>  
@@ -24,3 +43,5 @@
             <input type="reset" name='limpiar' value="Limpiar">
         </form>
     </main>
+</body>
+</html>

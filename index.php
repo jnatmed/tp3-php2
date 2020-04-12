436de9eee0e2ad9e0c7edbd3e8edca64da2adf6f
jnatmed <juanmannat@gmail.com>
2020-04-11 23:03:23 -0300

    Solucionado el tema del envio del turno confirmado. Solucion encontrada: tuve que color input tipo text con la informacion que ya estaba ingresada, pero con el tag hidden, osea ocultos . Mientras que en la tabla sigo mostrando los datos cargados al usuario. Y envio la informacion que deseo guardar. Todavia no solucione el tema del sobreescrito de las imagenes.

--- index.php
+++ index.php
@@ -7,14 +7,7 @@ $fController = new form_controller;
 $url_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $http_method = $_SERVER["REQUEST_METHOD"];
 
-// echo "<pre>";
-// var_dump($_SERVER);
-// var_dump($url_path);
-// var_dump($http_method);
-// exit(0);
-
-
-if ($url_path == '/save_formulario' && $http_method == 'GET'){
+if ($url_path == '/save_formulario' && $http_method == 'POST'){
     $fController->guardarFormulario();
 }else if($url_path == '/turno_confirmado' && $http_method == 'POST'){
     $fController->reservarTurno();
--- views/reserva.turno.view.php
+++ views/reserva.turno.view.php
@@ -19,22 +19,31 @@
                     <td>Horario del Turno</td>
                     <td>Receta Cargada</td>
                 </tr>
-                <?php foreach ($this->planilla as $turno):?>
-                    <tr>
-                        <td><?= $turno['nombre_paciente']; ?></td> 
-                        <td><?= $turno['email']; ?></td>
-                        <td><?= $turno['telefono'];?></td>
-                        <td><?= $turno['edad'];?></td>
-                        <td><?= $turno['talla_calzado'];?></td>
-                        <td><?= $turno['fecha_nacimiento'];?></td>
-                        <td><?= $turno['color_pelo'];?></td>
-                        <td><?= $turno['fecha_turno'];?></td>
-                        <td><?= $turno['hora_turno'];?></td> 
-                        <td><img src='<?= $turno['dir_img']; ?>' alt="imagen" width="500" height="333"></td>
-                    </tr>
-                <?php endforeach ?>
-            </table>
-            <input type="submit" name='enviar' value="Confirmar_Turno">
+                <?php $turno = $this->planilla[0];?>
+                <tr>
+                    <td><?= $turno['nombre_paciente']; ?></td> 
+                    <td><?= $turno['email']; ?></td>
+                    <td><?= $turno['telefono'];?></td>
+                    <td><?= $turno['edad'];?></td>
+                    <td><?= $turno['talla_calzado'];?></td>
+                    <td><?= $turno['fecha_nacimiento'];?></td>
+                    <td><?= $turno['color_pelo'];?></td>
+                    <td><?= $turno['fecha_turno'];?></td>
+                    <td><?= $turno['hora_turno'];?></td> 
+                    <td><img src='<?= $turno['dir_img']; ?>' alt="imagen" width="500" height="333"></td>
+                </tr>                                                        
+            </table><br><br>
+                <input type="text" value="<?= $turno['nombre_paciente']; ?>" name="<?= $turno['nombre_paciente']; ?>"hidden="true">
+                <input type="text" value="<?= $turno['email']; ?>" name="<?= $turno['email']; ?>"hidden="true">
+                <input type="text" value="<?= $turno['telefono']; ?>" name="<?= $turno['telefono']; ?>"hidden="true">
+                <input type="text" value="<?= $turno['edad']; ?>" name="<?= $turno['edad']; ?>"hidden="true">
+                <input type="text" value="<?= $turno['talla_calzado']; ?>" name="<?= $turno['talla_calzado']; ?>"hidden="true">
+                <input type="text" value="<?= $turno['fecha_nacimiento']; ?>" name="<?= $turno['fecha_nacimiento']; ?>"hidden="true">
+                <input type="text" value="<?= $turno['color_pelo']; ?>" name="<?= $turno['color_pelo']; ?>"hidden="true">
+                <input type="text" value="<?= $turno['fecha_turno']; ?>" name="<?= $turno['fecha_turno']; ?>"hidden="true">
+                <input type="text" value="<?= $turno['hora_turno']; ?>" name="<?= $turno['hora_turno']; ?>"hidden="true">
+                <input type="text" value="<?= $turno['dir_img']; ?>" name="<?= $turno['dir_img']; ?>" hidden="true">
+            <input type="submit" name='enviar' value="Confirmar Turno">
         </form>    
         <?php 
           if (!empty($this->datos_mal_cargados)){ ?>

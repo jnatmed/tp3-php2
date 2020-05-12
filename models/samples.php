4383ef97460fbb75e33c290456ed9d936e58bab8
jnatmed <juanmannat@gmail.com>
2020-05-12 15:03:23 -0300

    agregando funcion imagen a mysql..sin terminar

--- models/samples.php
+++ models/samples.php
@@ -1,7 +1,13 @@
 <?php
 
+// $img = imagecreatefromjpeg('img/giovi.jpeg');
+$fp = fopen($img,'r+b');
+$jpeg = fread($fp, filesize('img/giovi.jpeg'));
+fclose($fp);
+$jpeg = addslashes($jpeg);
+
 $sample_1 = [
-    "fecha_turno" => '2020-05-11',
+    "fecha_turno" => '2020-05-16',
     "hora_turno" => '08:48:04',
     "nombre_paciente" => 'juan alberto',
     "email" => 'pedromannat@gmail.com',
@@ -10,29 +16,32 @@ $sample_1 = [
     "edad" => '31',
     "talla_calzado" => '41',
     "altura" => '169',
-    "color_pelo" => 'castaño'   
+    "color_pelo" => 'castaño',
+    "imagen" => NULL    
 ];
 $sample_2 = [
-    "fecha_turno" => '2020-05-11',
-    "hora_turno" => '08:48:04',
-    "nombre_paciente" => 'juan alberto',
-    "email" => 'pedromannat@gmail.com',
-    "telefono" => '11-3438-7233',
+    "fecha_turno" => '2020-05-15',
+    "hora_turno" => '10:48:04',
+    "nombre_paciente" => 'juan ignacio',
+    "email" => 'juignacio@gmail.com',
+    "telefono" => '11-3434-7288',
     "fecha_nacimiento" => '1989-03-01',
     "edad" => '31',
     "talla_calzado" => '41',
     "altura" => '169',
-    "color_pelo" => 'castaño'   
+    "color_pelo" => 'rubio',   
+    "imagen" => NULL    
 ];
 $sample_3 = [
-    "fecha_turno" => '2020-05-11',
-    "hora_turno" => '08:48:04',
-    "nombre_paciente" => 'juan alberto',
-    "email" => 'pedromannat@gmail.com',
-    "telefono" => '11-3438-7233',
+    "fecha_turno" => '2020-05-20',
+    "hora_turno" => '11:15:04',
+    "nombre_paciente" => 'julio perez',
+    "email" => 'perezju@gmail.com',
+    "telefono" => '11-4566-9845',
     "fecha_nacimiento" => '1989-03-01',
     "edad" => '31',
     "talla_calzado" => '41',
     "altura" => '169',
-    "color_pelo" => 'castaño'   
+    "color_pelo" => 'negro',   
+    "imagen" => NULL    
 ];
